6fbd456210730d8a1d116f56206164bbf6a1dd8e
Andy671 <andreykotlaren@gmail.com>
2019-08-22 12:56:45 +0300

    FixBot

--- bot.js
+++ bot.js
@@ -2,15 +2,28 @@ const tmi = require('tmi.js')
 const fs = require('fs')
 const express = require('express')
 const moment = require('moment')
+const path = require('path')
+const readLastLines = require('read-last-lines')
+const countLinesInFile = require('count-lines-in-file')
+const util = require('util');
 const momentDurationFormatSetup = require("moment-duration-format")
 
+// Convert fs.readFile into Promise version of same    
+const readFile = util.promisify(fs.readFile)
+const appendFile = util.promisify(fs.appendFile)
+const writeFile = util.promisify(fs.writeFile)
+
 const app = express()
-const port = 3000
+const port = 3003
 
 const oathtoken = fs.readFileSync('./credentials/oathtoken.pass', { encoding: 'utf8' })
 const clientid = fs.readFileSync('./credentials/clientid.pass', { encoding: 'utf8' })
 const channelid = 206290135
-// Define configuration options
+const maxNumOfLines = 15
+
+const chatTxtPath = path.join(__dirname + '/chat.txt')
+const chatHtmlPath = path.join(__dirname + '/chat.html')
+
 const opts = {
   identity: {
     username: 'the_fix_bot',
@@ -21,24 +34,58 @@ const opts = {
   ]
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  let file = await readFile(chatHtmlPath, { encoding: 'utf8' })
+  let chat_file = await readFile(chatTxtPath, { encoding: 'utf-8' })
+  file = file.replace('REPLACE_ME', chat_file)
+  res.send(file)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+var handleScreenChat = async (context, msg) => {
+  let file = await readFile(chatTxtPath, { encoding: 'utf-8' })
+  let message = `<b>${context['display-name']}</b>: ${msg}`
+  let strings = splitter(message, 35)
+  let phrase_str = '<div>'
+  strings.forEach(str => {
+    phrase_str += `<span>${str}</span>\n`
+  })
+  phrase_str += '</div>'
+  let final_str = phrase_str + file
+  let final_str_arr = final_str.split('\n')
+  let numOfLines = final_str_arr.length
+  if (numOfLines > maxNumOfLines) {
+    final_str_arr = final_str_arr.slice(0, maxNumOfLines)
+  }
+  final_str = ''
+  final_str_arr.forEach(element => {
+    final_str += element + '\n'
+  })
+
+  writeFile(chatTxtPath, final_str, { encoding: 'utf-8' })
+}
+
+var splitter = (str, length) => {
+  let strs = []
+  while (str.length > length) {
+    var pos = str.substring(0, length).lastIndexOf(' ');
+    pos = pos <= 0 ? length : pos
+    strs.push(str.substring(0, pos))
+    var i = str.indexOf(' ', pos) + 1
+    if (i < pos || i > pos + length)
+      i = pos
+    str = str.substring(i)
+  }
+  strs.push(str)
+  return strs
+}
 
 // Called every time a message comes in
 var onMessageHandler = (target, context, msg, self) => {
+  handleScreenChat(context, msg)
+
   if (self) { return } // Ignore messages from the bot
-
-  // let message = `<${context['display-name']}>: ${msg}`
-  // let strings = splitter(message, 50)
-  // let append_str = ''
-  // strings.forEach(str => {
-  //   append_str += str + '\n'
-  // })
-
-  // fs.appendFile('chat.txt', append_str + '\n', () => {
-  //   console.log('Appended to file')
-  // })
 
   // Remove whitespace from chat message
   const commandName = msg.trim()
@@ -84,21 +131,6 @@ var onMessageHandler = (target, context, msg, self) => {
       break
   }
 
-}
-
-var splitter = (str, length) => {
-  let strs = []
-  while (str.length > length) {
-    var pos = str.substring(0, length).lastIndexOf(' ');
-    pos = pos <= 0 ? length : pos
-    strs.push(str.substring(0, pos))
-    var i = str.indexOf(' ', pos) + 1
-    if (i < pos || i > pos + length)
-      i = pos
-    str = str.substring(i)
-  }
-  strs.push(str)
-  return strs
 }
 
 // Called every time the bot connects to Twitch chat
